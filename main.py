e7239cfd9dd7e7786878bc69c0b03c4b79f2e372
Zsolt Ban <banzsolt94@gmail.com>
2024-07-01 17:11:53 +0100

    Added initial pages to flask

--- main.py
+++ main.py
@@ -10,5 +10,18 @@ def hello_world():
   name = os.environ.get("NAME", "World")
   return f"Hello {name}!"
 
+
+@app.route("/api/sme_dashboard")
+def sme_dashboard():
+  return f"This is api_sme_dashboard!"
+
+@app.route("/api/large_org_json")
+def sme_dashboard():
+  return f"This is api_large_dashboard!"
+
+@app.route("/dashboard")
+def sme_dashboard():
+  return f"This is dashboard!"
+
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
